36c7217de766896ea085c81af827ed82bb5f6cea
Daisuke Sasaki <gerund25495641@gmail.com>
2021-04-29 15:33:10 +0900

    配列を追加

--- pages/action.vue
+++ pages/action.vue
@@ -1,51 +1,32 @@
 <template>
-   <div class ="container"> HELLO WORLD!</div>
+  <div>
+   <div class ="container">{{name}}{{ world}}</div>
+    <button @click="changeTextHello()">{{btnNameHello}}</button>
+    <button @click="changeTextBye()">{{btnNameBye}}</button>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
-//表示のために必要な処理や、ボタンを押した際の処理を書くところ
+export default Vue.extend({
+    data: () => ({
+        name: 'こんにちは',
+        world: '世界',
+        btnNameHello: 'こんにちは',
+        btnNameBye: 'さようなら'
+    }),
+    methods: {
+        changeTextHello(){
+            this.name = "こんにちは"
+        },
+        changeTextBye(){
+            this.name = "さようなら"
+        },
+    },
+})
+
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
---
+++ pages/input.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <input type="text" v-model="newMessage" />
+        <br /><br />
+        <button @click ="addMessage()">追加</button>
+        <button @click ="deleteMessage()">古い順に削除</button>
+        <br /><br />
+        <div v-for="message in messages" :key ="message.index">
+             {{message}}
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+ 
+export default Vue.extend({
+  data: () => ({
+    newMessage: '',
+    messages: [] as any,
+  }),
+  methods: {
+    addMessage() {
+      this.messages.push(this.newMessage)
+      //pushで配列の要素を追加
+      this.newMessage = ''
+    },
+    deleteMessage() {
+      if (this.messages.length > 0) {
+        this.messages.splice(0, 1)
+        //spliceで配列の要素を削除
+      }
+    },
+  },
+})
+</script>
+ 
+<style></style>
